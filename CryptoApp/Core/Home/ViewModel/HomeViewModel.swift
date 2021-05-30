3c37a50ada5ece49f84b6519caad3a6b2a826c0f
Oleg Mihajlov <oleg@mkpwnz.moe>
2021-05-30 13:03:24 +0300

    Base HomeViewModel create

--- CryptoApp/Core/Home/ViewModel/HomeViewModel.swift
+++ CryptoApp/Core/Home/ViewModel/HomeViewModel.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import SwiftUI
+
+class HomeViewModel: ObservableObject {
+    @Published var allCoints: [Coin] = []
+    @Published var portfolioCoins: [Coin] = []
+    
+    init() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.allCoints.append(DeveloperPreview.shared.coin)
+            self.portfolioCoins.append(DeveloperPreview.shared.coin)
+        }
+    }
+}
--- CryptoApp/CryptoApp.swift
+++ CryptoApp/CryptoApp.swift
@@ -9,12 +9,15 @@ import SwiftUI
 
 @main
 struct CryptoApp: App {
+    @StateObject private var vm = HomeViewModel()
+    
     var body: some Scene {
         WindowGroup {
             NavigationView {
                 HomeView()
                     .navigationBarHidden(true)
             }
+            .wenvironmentObject(vm)
         }
     }
 }
