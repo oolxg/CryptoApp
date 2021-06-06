05201eaf739b492829068d9c42498c6fda367603
Oleg <oleg@mkpwnz.moe>
2021-06-06 19:53:24 +0300

    Add tracking of market data in HomeViewModel

--- CryptoApp/Core/Home/ViewModel/HomeViewModel.swift
+++ CryptoApp/Core/Home/ViewModel/HomeViewModel.swift
@@ -14,7 +14,10 @@ class HomeViewModel: ObservableObject {
     @Published var portfolioCoins: [Coin] = []
     @Published var searchText: String = ""
     
-    private let dataService = CoinDataService()
+    @Published var stats: [Statistic] = []
+    
+    private let coinDataService = CoinDataService()
+    private let marketDataService = MarketDataService()
     private var cancellables = Set<AnyCancellable>()
     
     init() {
@@ -22,11 +25,37 @@ class HomeViewModel: ObservableObject {
     }
     
     func addSubscribers() {
-        dataService
+        coinDataService
             .$allCoins
             .sink { [weak self] coins in
                 self?.allCoints = coins
             }
             .store(in: &cancellables)
+        
+        // update marketData
+        marketDataService
+            .$marketData
+            .map(mapGlobalMarketData)
+            .sink { [weak self] stats in
+                self?.stats = stats
+            }
+            .store(in: &cancellables)
+    }
+    
+    private func mapGlobalMarketData(marketDataModel: MarketData?) -> [Statistic] {
+        var stats: [Statistic] = []
+        
+        guard let data = marketDataModel else {
+            return stats
+        }
+        
+        let marketCap = Statistic(title: "Market Cap", value: data.marketCap, percentageChange: data.marketCapChangePercentage24HUsd)
+        let volume = Statistic(title: "24h Volume", value: data.volume)
+        let btcDominance = Statistic(title: "BTC Dominance", value: data.btcDominance)
+        let portfolioValue = Statistic(title: "Portfolio Value", value: "$0.00")
+        
+        stats.append(contentsOf: [marketCap, volume, btcDominance, portfolioValue])
+        
+        return stats
     }
 }
