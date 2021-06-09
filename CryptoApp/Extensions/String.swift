0f5d061ae62964d4bfd736eed16b099e417bbe5d
Oleg <oleg@mkpwnz.moe>
2021-06-09 16:38:13 +0300

    Create extension to convert String to Double

---
+++ CryptoApp/Extensions/String.swift
@@ -0,0 +1,33 @@
+//
+//  String.swift
+//  CryptoApp
+//
+//  Created by mk.pwnz on 09.06.2021.
+//
+
+import Foundation
+
+extension String {
+    /// Convert a `String` to a `Double`
+    /// ```
+    /// Convert "12" to 12.00
+    /// Convert "-12" to -12.00
+    /// Convert "123abcde4" to nil
+    /// ```
+    func asDouble() -> Double? {
+        let formatter = NumberFormatter()
+        
+        formatter.decimalSeparator = "."
+        
+        if let result = formatter.number(from: self) {
+            return result.doubleValue
+        } else {
+            formatter.decimalSeparator = ","
+            if let result = formatter.number(from: self) {
+                return result.doubleValue
+            }
+        }
+        
+        return nil
+    }
+}
