6e1edcb25f7b2c1a7a8942dca4c73fdbd19fcb3c
Oleg <oleg@mkpwnz.moe>
2021-06-09 18:11:07 +0300

    Add overrides for LargeTitle color

--- CryptoApp/CryptoApp.swift
+++ CryptoApp/CryptoApp.swift
@@ -11,6 +11,11 @@ import SwiftUI
 struct CryptoApp: App {
     @StateObject private var vm = HomeViewModel()
     
+    init() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor(.theme.accent)]
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor(.theme.accent)]
+    }
+    
     var body: some Scene {
         WindowGroup {
             NavigationView {
