5e76263f21db2292ba60e645773a8f2752aedebd
Oleg <oleg@mkpwnz.moe>
2021-06-11 16:54:20 +0300

    Change getCoins access level. Private->Public

--- CryptoApp/Services/MarketDataService.swift
+++ CryptoApp/Services/MarketDataService.swift
@@ -17,7 +17,7 @@ class MarketDataService {
         getData()
     }
     
-    private func getData() {
+    func getData() {
         guard let url = URL(string: "https://api.coingecko.com/api/v3/global")
         else { return }
         
