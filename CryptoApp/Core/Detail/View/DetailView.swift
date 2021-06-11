f03ac18afb2784eb013f78d1cd3acbf364eca015
Oleg <oleg@mkpwnz.moe>
2021-06-11 20:27:24 +0300

    Create the most basics of DetailView for coin

---
+++ CryptoApp/Core/Detail/View/DetailView.swift
@@ -0,0 +1,35 @@
+//
+//  DetailView.swift
+//  CryptoApp
+//
+//  Created by mk.pwnz on 11.06.2021.
+//
+
+import SwiftUI
+
+struct DetailLoadingView: View {
+    @Binding var coin: Coin?
+    
+    var body: some View {
+        ZStack {
+            if let coin = coin {
+                DetailView(coin: coin)
+            }
+        }
+    }
+}
+
+struct DetailView: View {
+    
+    let coin: Coin
+    
+    var body: some View {
+        Text(coin.name)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(coin: dev.coin)
+    }
+}
