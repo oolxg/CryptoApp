b66eba1ac4e6ac99ee19b21b6c1761023b3acda9
Oleg <oleg@mkpwnz.moe>
2021-06-13 00:44:13 +0300

    Create service for fetching detailed data from API for Coin

---
+++ CryptoApp/Services/CoinDetailDataService.swift
@@ -0,0 +1,36 @@
+//
+//  CoinDetailDataService.swift
+//  CryptoApp
+//
+//  Created by mk.pwnz on 12.06.2021.
+//
+
+import Foundation
+import Combine
+
+class CoinDetailDataService {
+    let coin: Coin
+    
+    @Published var coinDetail: CoinDetail? = nil
+    
+    private var coinDetailSubscription: AnyCancellable?
+    
+    init(coin: Coin) {
+        self.coin = coin
+        getCoinDetails()
+    }
+    
+    func getCoinDetails() {
+        guard let url = URL(string: "ht    https://api.coingecko.com/api/v3/coins/\(coin.id)?localization=false&tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=false")
+        else { return }
+        
+        coinDetailSubscription = NetworkManager.download(from: url)
+            .decode(type: CoinDetail.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkManager.handleCompletion,
+                  receiveValue: { [weak self] returndeCoinDetail in
+                    guard let self = self else { return }
+                    self.coinDetail = returndeCoinDetail
+                    self.coinDetailSubscription?.cancel()
+                  })
+    }
+}
