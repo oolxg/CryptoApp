b159c507aee34653ebf86da20ce482d2cf74e12c
Oleg <oleg@mkpwnz.moe>
2021-06-13 22:14:34 +0300

    Getting data from DetailServce and transform to View

--- CryptoApp/Core/Detail/ViewModel/DetailViewModel.swift
+++ CryptoApp/Core/Detail/ViewModel/DetailViewModel.swift
@@ -11,7 +11,10 @@ import Combine
 class DetailViewModel: ObservableObject {
     private let coinDetailService: CoinDetailDataService
     private var cancellables = Set<AnyCancellable>()
-    private let coin: Coin
+    @Published var coin: Coin
+    
+    @Published var overviewStatistic: [Statistic] = []
+    @Published var detailStatistic: [Statistic] = []
     
     init(coin: Coin) {
         self.coin = coin
@@ -22,9 +25,76 @@ class DetailViewModel: ObservableObject {
     
     private func addSubscribers() {
         coinDetailService.$coinDetail
-            .sink { returnedCoinDetails in
-                print(returnedCoinDetails)
+            .combineLatest($coin)
+            .map(mapDataToStatistic)
+            .sink { [weak self] returnedArrays in
+                guard let self = self else { return }
+                self.overviewStatistic = returnedArrays.overview
+                self.detailStatistic = returnedArrays.details
             }
             .store(in: &cancellables)
+    }
+    
+    private func mapDataToStatistic(coinDetail: CoinDetail?, coin: Coin) -> (overview: [Statistic], details: [Statistic]) {
+        let overview = createOverview(coinDetail: coinDetail)
+        
+        let details = createDetailsForCoin(coin: coin, coinDetail: coinDetail)
+        
+        return (overview, details)
+    }
+    
+    private func createOverview(coinDetail: CoinDetail?) -> [Statistic] {
+        let price = coin.currentPrice.asCurrencyWith6Decimals()
+        let pricePercentChange = coin.priceChange24H
+        let priceStat = Statistic(title: "Current Price", value: price, percentageChange: pricePercentChange)
+        
+        let marketCap = "$" + (coin.marketCap?.formattedWithAbbreviations() ?? "")
+        let marketCapPercentChange = coin.marketCapChangePercentage24H
+        let marketCapStat = Statistic(title: "Market Cap", value: marketCap, percentageChange: marketCapPercentChange)
+        
+        let rank = "\(coin.rank)"
+        let rankStat = Statistic(title: "Rank", value: rank)
+        
+        let volume = "$" + "\(coin.totalVolume?.formattedWithAbbreviations() ?? "")"
+        let volumeStat = Statistic(title: "Volume", value: volume)
+        
+        return [
+            priceStat,
+            marketCapStat,
+            rankStat,
+            volumeStat
+        ]
+    }
+    
+    private func createDetailsForCoin(coin: Coin, coinDetail: CoinDetail?) -> [Statistic] {
+        let high = coin.high24H?.asCurrencyWith6Decimals() ?? "N/A"
+        let highStat = Statistic(title: "High 24h", value: high)
+        
+        let low = coin.low24H?.asCurrencyWith6Decimals() ?? "N/A"
+        let lowStat = Statistic(title: "Low 24h", value: low)
+        
+        let priceChange = coin.priceChange24H?.asCurrencyWith6Decimals() ?? "N/A"
+        let pricePercentChange = coin.priceChange24H
+        let priceChangeStat = Statistic(title: "24h Price Change", value: priceChange, percentageChange: pricePercentChange)
+        
+        let marketCapChange = "$" + (coin.marketCapChange24H?.formattedWithAbbreviations() ?? "")
+        let marketCapPercentChange = coin.marketCapChangePercentage24H
+        let marketCapChangeStat = Statistic(title: "24h Market Cap Change", value: marketCapChange, percentageChange: marketCapPercentChange)
+        
+        let blockTime = coinDetail?.blockTimeInMinutes ?? 0
+        let blockTimeString = blockTime == 0 ? "N/A" : "\(blockTime)"
+        let blockTimeStat = Statistic(title: "Block Time", value: blockTimeString)
+        
+        let hashingAlgo = coinDetail?.hashingAlgorithm ?? "N/A"
+        let hashingAlgoStat = Statistic(title: "Hashing Algorithm", value: hashingAlgo)
+        
+        return [
+            highStat,
+            lowStat,
+            priceChangeStat,
+            marketCapChangeStat,
+            blockTimeStat,
+            hashingAlgoStat
+        ]
     }
 }
