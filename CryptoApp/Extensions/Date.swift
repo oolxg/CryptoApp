4e94c5b3384385671f7aa2c06202018219ee46cb
Oleg <oleg@mkpwnz.moe>
2021-06-14 19:36:06 +0300

    Add short formatter from Date to String. Add init from CoinGecko string to Date

---
+++ CryptoApp/Extensions/Date.swift
@@ -0,0 +1,33 @@
+//
+//  Date.swift
+//  CryptoApp
+//
+//  Created by mk.pwnz on 14.06.2021.
+//
+
+import Foundation
+
+
+extension Date {
+    //  "2013-07-06T00:00:00.000Z"
+    init(coinGeckoString: String) {
+        let formatter = DateFormatter()
+        
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        
+        let date = formatter.date(from: coinGeckoString) ?? Date()
+        self.init(timeInterval: 0, since: date)
+    }
+    
+    private var shortFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        
+        formatter.dateStyle = .short
+        
+        return formatter
+    }
+    
+    func asShortDateString() -> String {
+        shortFormatter.string(from: self)
+    }
+}
