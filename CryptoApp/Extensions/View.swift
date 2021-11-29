532403e36a8a3bc9ce8292c5d065074c0df4303f
Oleg <oleg@mkpwnz.moe>
2021-11-29 01:44:17 +0100

    Add invisible modifier to View

---
+++ CryptoApp/Extensions/View.swift
@@ -0,0 +1,18 @@
+//
+//  View.swift
+//  CryptoApp
+//
+//  Created by mk.pwnz on 29.11.2021.
+//
+
+import Foundation
+import SwiftUI
+
+
+extension View {
+    func invisible(_ isVisible: Bool = false) -> some View {
+        self
+            .opacity(isVisible ? 1 : 0)
+            .disabled(isVisible)
+    }
+}
