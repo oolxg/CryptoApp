893eff49ef2a7b4123b9adaf990045dc683837f2
Oleg <oleg@mkpwnz.moe>
2022-01-08 18:00:00 +0100

    Fix typo in dir name

---
+++ CryptoApp/Utilities/KeyChain/KeyChainPropertyWrapper.swift
@@ -0,0 +1,38 @@
+//
+//  KeyChainPropertyWrapper.swift
+//  CryptoApp
+//
+//  Created by mk.pwnz on 08/01/2022.
+//
+
+import Foundation
+import SwiftUI
+
+@propertyWrapper
+struct KeyChain: DynamicProperty {
+    @State var data: Data?
+    
+    var wrappedValue: Data? {
+        get { data }
+        set {
+            guard let newData = newValue else {
+                // if setting data to `nil`
+                // delete it from the Key Chain
+                data = nil
+                KeyChainHelper.shared.delete(key: key, account: account)
+                return
+            }
+            KeyChainHelper.shared.save(data: newData, forKey: key, account: account)
+        }
+    }
+    
+    var key: String
+    var account: String
+    
+    init(key: String, account: String) {
+        self.key = key
+        self.account = account
+        
+        _data = State(wrappedValue: KeyChainHelper.shared.read(key: key, forAccount: account))
+    }
+}
