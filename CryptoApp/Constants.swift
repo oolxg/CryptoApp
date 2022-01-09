1aa31d444eaa1db208cdde70b92ce2b93ac6234e
Oleg <oleg@mkpwnz.moe>
2022-01-09 14:16:07 +0100

    Add constants class

---
+++ CryptoApp/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  CryptoApp
+//
+//  Created by mk.pwnz on 08/01/2022.
+//
+
+import Foundation
+
+
+struct Constants {
+    struct KeyChain {
+        static let account: String = "CryptoApp"
+        static let pincodeKey: String = "user_pincode"
+    }
+}
